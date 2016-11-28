eda8a863070f0b31867d4cfb53b6d70ffd85c865
Jason Zheng <one@fish.cat>
2016-11-27 21:34:27 -0500

    Add files via upload

---
+++ Chapter1Projects.java
@@ -0,0 +1,40 @@
+//Jason Zheng's Chapter 1 Programming Projects 
+
+//Below are the projects 1.1, 1.2, 1.3, 1.9 in order (1.10 is already sent through email)
+
+public class Chapter1Projects
+{
+  public static void main(String[] args)
+  {
+    
+    //Project 1.1 (I added newline to space out the projects in the output space)
+    System.out.println("An Emergency Broadcast\n");
+    
+    
+    //Project 1.2 is on email
+    
+    //Project 1.3
+    System.out.println("Name: Jason Zheng");
+    System.out.println("Hobbies: Playing piano, practicing tennis, doing the laundry");
+    System.out.println("Favorite Book: The Adventures of Dr. Dolittle");
+    System.out.println("Favorite Movie: Life of Pi\n");
+    
+    //Project 1.9 
+    System.out.println("    *    ");
+    System.out.println("   ***   ");
+    System.out.println("  *****  ");
+    System.out.println(" ******* ");
+    System.out.println("*********");
+    System.out.println(" ******* ");
+    System.out.println("  *****  ");
+    System.out.println("   ***   ");
+    System.out.println("    *    ");
+    
+    
+   
+  }
+}
+
+
+
+
---
+++ QPP2.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+
+public class QPP2 {
+ 
+  public static void main(String args[]) {
+    Scanner reader = new Scanner(System.in);
+    String name, college, petName;
+    int age, n, d;
+    double avg, amount, oneDollar, fiveDollar, tenDollar, penny, nickel, dime, quarter, decimal;
+    
+    //PP 2.2
+    System.out.print("Enter an integer: ");
+    int x = reader.nextInt();
+    System.out.print("Enter an integer: ");
+    int y = reader.nextInt();
+    System.out.print("Enter an integer: ");
+    int z = reader.nextInt();
+    
+    avg = (double) (x + y + z) / 3;
+    
+    System.out.println("The average of your three integers is: " + avg);
+    
+    //PP 2.3 
+    System.out.print("Enter a decimal: ");
+    float a = reader.nextFloat();
+    System.out.print("Enter a decimal: ");
+    float b = reader.nextFloat();
+    
+    System.out.println("The sum is: " + (a+b) + ", the product is " + (a*b) + ", and the difference is " + (a-b));
+    
+    //PP 2.4
+    name = reader.nextLine(); //the press of enter messes up the flow
+    System.out.print("What is your name? ");
+    name = reader.nextLine();
+    System.out.print("What is your age?: ");
+    age = Integer.parseInt(reader.nextLine());
+
+    System.out.print("What is your college?: ");
+    college = reader.nextLine();
+    System.out.print("What is your pet's name?: ");
+    petName = reader.nextLine();
+
+    System.out.println("Hello, my name is " + name + " and I am " + age
+                + " years \nold. I\'m enjoying my time at " + college
+                + ", though \nI miss my pet " + petName + " very much!");
+        
+     //PP 2.7
+     System.out.println("Enter the distance you have traveled in miles?");
+     int distance = Integer.parseInt(reader.nextLine());
+     System.out.println("Enter the speed you traveled in mph?");
+     int speed = Integer.parseInt(reader.nextLine());
+     float time = (float) distance / speed;
+
+     System.out.println("The time it took you to travel " + distance + " miles at a speed of " + speed + " mph is " + time + " hours.");
+     
+     //PP 2.11
+     System.out.print("What is the monetary amount?: ");
+     amount = reader.nextDouble();
+     
+     tenDollar = amount / 10;
+     amount = amount %10;
+
+     fiveDollar = amount / 5;
+     amount = amount % 5;
+
+     oneDollar = amount / 1;
+     amount = amount % 1;
+     
+     quarter = amount / .25;
+     amount = amount % .25;
+     
+     dime = amount / .10;
+     amount = amount % .10;
+     
+     nickel = amount / .05;
+     amount = amount % .05;
+     
+     penny = amount / .01;
+     amount = amount % .01;
+     
+     System.out.println((int)tenDollar + " ten dollar bills");
+     System.out.println((int)fiveDollar + " five dollar bils");
+     System.out.println((int)oneDollar + " one dollar bills");
+     System.out.println((int)quarter + " quarters");
+     System.out.println((int)dime + " dimes ");
+     System.out.println((int)nickel + " nickels");
+     System.out.println((int)penny + " pennies");
+     
+     //2.13
+     System.out.println("Numerator?: ");
+     n = reader.nextInt();
+     System.out.println("Denominator?: ");
+     d = reader.nextInt();
+     
+     decimal = (double) n / d;
+     
+     System.out.println("The decimal equivalent of the fraction is " + decimal);
+     
+    
+     
+    
+        
+  }
+}
